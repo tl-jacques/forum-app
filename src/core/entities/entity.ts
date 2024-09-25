5a17c5c373e385a43a11790199181856d9cbedcb
Thiago Jacques <thiagolinhaes9@gmail.com>
2024-09-25 10:14:59 -0300

    core base entities created

---
+++ src/core/entities/entity.ts
@@ -0,0 +1,16 @@
+import { UniqueEntityID } from './unique-entity-id'
+
+export class Entity<Props> {
+  private _id: UniqueEntityID
+  protected props: any
+
+  get id() {
+    return this._id
+  }
+
+  protected constructor(props: Props, id?: UniqueEntityID) {
+    this.props = props
+    this._id = id ?? new UniqueEntityID()
+  }
+}
+ 
