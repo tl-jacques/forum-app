1fe5db733ff3cd98845a35c26597f83736e8a18c
Thiago Jacques <thiagolinhaes9@gmail.com>
2024-09-27 09:22:29 -0300

    notification: created methods `save` and `findById`

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -11,5 +11,20 @@ export class InMemoryNotificationsRepository implements NotificationsRepository 
   }
 
 
+  async findById(id: string): Promise<Notification | null> {
+    const notification = this.items.find(item => item.id.toString() === id)
+
+    if (!notification) {
+      return null
+    }
+
+    return notification
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const itemIndex = this.items.findIndex(item => item.id === notification.id)
+
+    this.items[itemIndex] = notification
+  }
 
 }
